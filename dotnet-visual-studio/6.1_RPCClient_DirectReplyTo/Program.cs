a4ceb7250011f6142415c2403c168ddd67853a95
gavinc <chrisgavin@outlook.com.au>
2018-06-07 22:54:47 +1000

    Add RPC version using Direct-ReplyTo pattern.

---
+++ dotnet-visual-studio/6.1_RPCClient_DirectReplyTo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class Program
+{
+    public static void Main( string[] args )
+    {
+        while (true)
+        {
+            var rpcClient = new RPCClient();
+
+            var n = args.Length > 0 ? args[0] : "30";
+            Console.WriteLine(" [x] Requesting fib({0})", n);
+            var response = rpcClient.Call(n);
+            Console.WriteLine(" [.] Got '{0}'", response);
+            rpcClient.Close();
+
+            Console.WriteLine(" Press [enter] to exit, any other key to continue with a new request.");
+            if (Console.ReadKey().Key == ConsoleKey.Enter) {
+                break;
+            }
+        }
+    }
+}
---
+++ dotnet-visual-studio/6.1_RPCClient_DirectReplyTo/RPCClient.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class RPCClient
+{
+    private IConnection connection;
+    private IModel channel;
+    private string replyQueueName;
+    private QueueingBasicConsumer consumer;
+
+    public RPCClient()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+        channel.ExchangeDeclare("Test", "direct");
+        replyQueueName = "amq.rabbitmq.reply-to";
+        consumer = new QueueingBasicConsumer(channel);
+        channel.BasicConsume(queue: replyQueueName, autoAck: true, consumer: consumer);
+    }
+
+    public string Call(string message)
+    {
+        var corrId = Guid.NewGuid().ToString();
+        var props = channel.CreateBasicProperties();
+        props.ReplyTo = replyQueueName;
+        props.CorrelationId = corrId;
+
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(exchange: "Test", routingKey: "", basicProperties: props, body: messageBytes);
+
+        BasicDeliverEventArgs args;
+        if (consumer.Queue.Dequeue(5000, out args))
+            return Encoding.UTF8.GetString(args.Body);
+
+        return string.Empty;
+    }
+
+    public void Close()
+    {
+        connection.Close();
+    }
+}
